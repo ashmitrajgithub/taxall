986383a8e224a3e5e1e0bec513f2bd3a228d5448
Ashmit Raj <trivediashmit3@gmail.com>
2025-02-10 00:59:35 +0530

    03

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "./components/header/Header"; // Correct import
-
+import Home from "./components/home/Home";
+import Footer from "./components/footer/Footer";
 const App = () => {
   return (
     <div>
       <Header />
-      <main className="p-4">
-        <h2>Welcome to our website!</h2>
-        <p>Explore our products and resources.</p>
-      </main>
+      <Home />
+      <Footer />
     </div>
   );
 };
