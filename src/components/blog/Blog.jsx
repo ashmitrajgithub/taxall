16da26e4145ffcd44f85a991c7821a4c26d1bfda
Ashmit Raj <trivediashmit3@gmail.com>
2025-02-11 22:34:52 +0530

    hii

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './Blog.css';
 
 const Blog = () => {
-  // Data for the latest blog post
+  // Data for the latest blog post (with image)
   const latestBlog = {
     title: "Latest Blog Post",
     image: "assets/01.jpg",
@@ -12,20 +12,20 @@ const Blog = () => {
     date: "Feb 10, 2025",
   };
 
-  // Array of other blog posts
+  // Array of blog posts with images
   const otherBlogs = [
     {
       id: 1,
       title: "Blog Post 1",
-      image: "assets/2.jpg",
+      image: "assets/02.jpg",
       description:
         "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
       date: "Feb 08, 2025",
     },
     {
       id: 2,
-      title: "Blog Post 2",
-      image: "assets/02.jpg",
+      title: "assets/2.jpg",
+      image: "assets/2.jpg",
       description:
         "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.",
       date: "Feb 05, 2025",
@@ -40,11 +40,29 @@ const Blog = () => {
     },
   ];
 
+  // Array of text-only blog posts (without images)
+  const textBlogs = [
+    {
+      id: 1,
+      title: "Text Blog Post 1",
+      content:
+        "This is a text-only blog post. It focuses entirely on the narrative without any accompanying image. The content is designed to be engaging purely through words.",
+      date: "Feb 12, 2025",
+    },
+    {
+      id: 2,
+      title: "Text Blog Post 2",
+      content:
+        "Another insightful text blog entry that provides deep analysis and commentary. Enjoy the uninterrupted flow of text that tells its story without any visual distractions.",
+      date: "Feb 11, 2025",
+    },
+  ];
+
   return (
     <div className="blog-container">
       <h1 className="blog-title">Our Blog</h1>
 
-      {/* Latest Blog Section */}
+      {/* Latest Blog Section (Big and Prominent) */}
       <section className="latest-blog">
         <h2 className="section-title">Latest Blog</h2>
         <div className="blog-card latest">
@@ -61,7 +79,7 @@ const Blog = () => {
         </div>
       </section>
 
-      {/* More Blogs Section */}
+      {/* More Blogs Section (With Images) */}
       <section className="other-blogs">
         <h2 className="section-title">More Blogs</h2>
         <div className="blog-grid">
@@ -73,6 +91,20 @@ const Blog = () => {
                 <p>{blog.description}</p>
                 <span className="blog-date">{blog.date}</span>
               </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Text Blogs Section (Without Images) */}
+      <section className="text-blogs">
+        <h2 className="section-title">Text Blogs</h2>
+        <div className="text-blog-grid">
+          {textBlogs.map((blog) => (
+            <div key={blog.id} className="text-blog-item">
+              <h3>{blog.title}</h3>
+              <p>{blog.content}</p>
+              <span className="blog-date">{blog.date}</span>
             </div>
           ))}
         </div>
